1eee54085b6207aed48885615789ec1ff3827727
User <lyamzin01@mail.ru>
2023-11-04 08:19:55 +0300

    gotovo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
-public class Main {
+public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1(); task2(); task3();
+    }
+
+    public static void task1() {
+
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich ";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
+
+    }
+
+    public static void task2() {
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich ";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + middleName;
+
+        System.out.println();
+
+
+        String fullNameUpperCase = fullName.toUpperCase();
+        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчета - "
+                + fullNameUpperCase);
+        // Второй метод:
+
+        System.out.println();
+        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчета - "
+        + fullName.toUpperCase());
+
+    }
+    public static void task3(){
+
+    String fullName = "Иванов Семён Семёнович";
+
+        System.out.println("Данные Ф.И.О. сотрудника "
+                + fullName.replace("ё", "е"));
+
     }
 }
+
